5bd636d347d7664946c8b1dbf2c0068aca1f1847
aradhita2001 <aradhita2001@gmail.com>
2023-08-10 00:56:44 +0530

    solved

---
+++ 2357/Solution.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+
+class Solution {
+    public int minimumOperations(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<Integer>();
+
+        for (int num : nums) {
+            if (num != 0)
+                set.add(num);
+        }
+
+        return set.size();
+    }
+}
